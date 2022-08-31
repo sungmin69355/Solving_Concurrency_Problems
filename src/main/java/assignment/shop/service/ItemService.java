716b3fdb69b577df22b97c95b610c781bd5b146e
sungmin69355 <sungmin69355@naver.com>
2022-08-31 21:11:22 +0900

    refactor: 상품조회 시 cache를 통해서 성능 개선 10ms -> 5ms

--- src/main/java/assignment/shop/service/ItemService.java
+++ src/main/java/assignment/shop/service/ItemService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.cache.annotation.Cacheable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,6 +16,7 @@ import java.util.List;
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    @Cacheable(cacheNames = "getItems", key = "#displayDate.toLocalDate().toString()")
     public List<Item> findDisplayDate(LocalDateTime displayDate){
         return itemRepository.findDisplayDate(displayDate);
     }
